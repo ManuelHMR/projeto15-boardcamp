9d06da26d58dc3ac3381e446bf82f1204caedcca
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-29 17:57:12 -0300

    customers controllers

--- controllers/customersControllers.js
+++ controllers/customersControllers.js
@@ -1,20 +1,48 @@
 import connection from "../database.js";
 
 export async function getCustomers(req, res){
+    const { cpf } = req.query;
+    if(!cpf){
+        try {
+            const result =  await connection.query(`
+                SELECT *
+                FROM customers;`
+            )
+            return res.send(result.rows);
+        } catch (err){
+            return res.send(err);
+        }
+    }
+    try{
+        const result =  await connection.query(`
+                SELECT *
+                FROM customers
+                WHERE cpf
+                LIKE $1;`, [`${cpf}%`]
+            )
+            res.send(result.rows);
+    }catch (err){
+        res.send(err);
+    }    
+};
+
+export async function getCustomersById (req,res){
+    const { id } = req.params;
     try {
         const result =  await connection.query(`
             SELECT *
-            FROM customers;`
+            FROM customers
+            WHERE id=$1;`, [id]
         )
-        return res.send(result.rows);
+        const { rows } = result;
+        if(rows.length === 0){
+            res.sendStatus(404)
+        }
+        return res.send(result.rows[0]);
     } catch (err){
         return res.send(err);
     }
 };
-
-export async function getCustomersById (req,res){
-
-}
 
 export async function postCustomers(req, res){
     const {name, phone, cpf, birthday} = req.body;
@@ -30,5 +58,23 @@ export async function postCustomers(req, res){
 };
 
 export async function putCustomers (req, res){
-
-}
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+    try {
+        const result = await connection.query(`
+        UPDATE customers 
+        SET (name, phone, cpf, birthday) = ( $1, $2, $3, $4) 
+        WHERE id = $5;`,
+            [
+                name,
+                phone,
+                cpf,
+                birthday,
+                id
+            ]
+        );
+        return res.sendStatus(201);
+    } catch (err){
+        res.send(err);
+    }
+};
